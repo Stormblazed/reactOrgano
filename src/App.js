258efc5b00c0e6d0d4c2851aba765b00514d4927
Stormblazed <wallacelive1@hotmail.com>
2023-10-22 11:50:43 -0300

    5º lession finish

--- src/App.js
+++ src/App.js
@@ -45,8 +45,7 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState([])
 
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
+  const aoNovoColaboradorAdicionado = (colaborador) => {    
     setColaboradores([...colaboradores, colaborador])
   }
 
